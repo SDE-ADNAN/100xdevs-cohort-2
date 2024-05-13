53364e8d9de342580a46a446cd03dbaa0fb05261
SDE-ADNAN <adnansdeofficial@gmail.com>
2024-05-13 14:49:50 +0530

    added usecases of atoms and selectors of recoil

--- week-7.4/src/App.jsx
+++ week-7.4/src/App.jsx
@@ -1,10 +1,29 @@
+import { RecoilRoot, useRecoilValue } from 'recoil'
 import './App.css'
+import { jobsAtom, messagingAtom, networkAtom, notificationAtom, totalNotificationSelector } from './atoms'
 
 function App() {
+  return <RecoilRoot><MainApp /></RecoilRoot>
+}
 
+function MainApp() {
+
+  const networkNotificationCount = useRecoilValue(networkAtom)
+  const finalValue = networkNotificationCount >= 100 ? "99+" : networkNotificationCount;
+
+  const jobsAtomCount = useRecoilValue(jobsAtom)
+  const notificationAtomCount = useRecoilValue(notificationAtom)
+  const messagingAtomCount = useRecoilValue(messagingAtom)
+
+  const totalNotificationCount = useRecoilValue(totalNotificationSelector)
   return (
     <>
-      
+      <button>Home</button>
+      <button>My Network ({finalValue})</button>
+      <button>Jobs ({jobsAtomCount})</button>
+      <button>Messaging ({messagingAtomCount})</button>
+      <button>Notification ({notificationAtomCount})</button>
+      <button>Me ({totalNotificationCount})</button>
     </>
   )
 }
