0f293ab19d201cab33266e1450243022aebb8b87
SDE-ADNAN <adnansdeofficial@gmail.com>
2024-06-30 19:35:54 +0530

    done with deploying prisma accelerate + cloudflare worker app

--- week-12-6/src/index.ts
+++ week-12-6/src/index.ts
@@ -11,8 +11,33 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+import { ExecutionContext, Request } from "@cloudflare/workers-types/experimental";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from '@prisma/extension-accelerate'
+
+export interface Env {
+	DATABASE_CONNECTION_POOL_URL: string
+}
+
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+	async fetch(
+		req: Request,
+		env: Env,
+		ctx: ExecutionContext
+	): Promise<Response> {
+		const prisma = new PrismaClient({
+			datasourceUrl: env.DATABASE_CONNECTION_POOL_URL
+		}).$extends(withAccelerate())
+		const response = await prisma.log.create({
+			data: {
+				level: 'info',
+				message: `${req.method} ${req.url}`,
+				meta: {
+					headers: JSON.stringify(req.headers)
+				}
+			}
+		})
+		console.log(JSON.stringify(response))
+		return Response.json(response)
+	}
+}
