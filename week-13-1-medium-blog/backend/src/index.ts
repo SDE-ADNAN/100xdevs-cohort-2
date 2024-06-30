065aa3a14ba281751c9109c48fa93c821d82164f
SDE-ADNAN <adnansdeofficial@gmail.com>
2024-07-01 00:27:32 +0530

    added a package and also did wrote a route to handle the signup requests

--- week-13-1-medium-blog/backend/src/index.ts
+++ week-13-1-medium-blog/backend/src/index.ts
@@ -1,8 +1,27 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
+import {PrismaClient} from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate';
 
-const app = new Hono()
 
-app.get('/api/v1/signup', (c) => {
+// this below line is required to let hono know that we have a key named DATABASE_URL which must be a string
+const app = new Hono<{
+  Bindings:{
+    DATABASE_URL:string
+  }
+}>
+
+app.get('/api/v1/signup', async (c) => {
+  const prisma= new PrismaClient({
+    datasourceUrl:c.env.DATABASE_URL
+  }).$extends(withAccelerate())
+
+  const body = await c.req.json()
+  await prisma.user.create({
+    data:{
+      email:body.email,
+      password:body.password
+    }
+  })
   return c.text('Hello Hono!')
 })
 
