1eff6e003b801be65f4862ef3b70e6c7909f53c3
SDE-ADNAN <adnansdeofficial@gmail.com>
2024-07-08 23:27:41 +0530

    blogs main page component

---
+++ week-13-1-medium-blog/frontend/src/pages/Blogs.tsx
@@ -0,0 +1,38 @@
+import BlogCard from "../components/BlogCard"
+
+
+const Blogs = () => {
+  return (
+    <div className="flex justify-center py-8">
+        <div className="max-w-xl">
+        <BlogCard 
+        authorName="Adnan Khan"
+        title="How an ugly single page website maeks $5000 a month without affiliate marketting"
+        content="How an ugly single page website maeks $5000 a month without affiliate markettingHow an ugly single page website maeks $5000 a month without affiliate marketting"
+        publishedDate="8th july 2024"
+        />
+        <BlogCard 
+        authorName="Adnan Khan"
+        title="How an ugly single page website maeks $5000 a month without affiliate marketting"
+        content="How an ugly single page website maeks $5000 a month without affiliate markettingHow an ugly single page website maeks $5000 a month without affiliate marketting"
+        publishedDate="8th july 2024"
+        />
+        <BlogCard 
+        authorName="Adnan Khan"
+        title="How an ugly single page website maeks $5000 a month without affiliate marketting"
+        content="How an ugly single page website maeks $5000 a month without affiliate markettingHow an ugly single page website maeks $5000 a month without affiliate marketting"
+        publishedDate="8th july 2024"
+        />
+        <BlogCard 
+        authorName="Adnan Khan"
+        title="How an ugly single page website maeks $5000 a month without affiliate marketting"
+        content="How an ugly single page website maeks $5000 a month without affiliate markettingHow an ugly single page website maeks $5000 a month without affiliate marketting"
+        publishedDate="8th july 2024"
+        />
+    </div>
+    </div>
+    
+  )
+}
+
+export default Blogs
