525f8ce0b72541835221efcb87b6a873c368ed67
SDE-ADNAN <adnansdeofficial@gmail.com>
2024-07-08 23:27:29 +0530

    new blogs page added

--- week-13-1-medium-blog/frontend/src/App.tsx
+++ week-13-1-medium-blog/frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Signin from './pages/SignIn'
 import Blog from './pages/Blog'
 import Signup from './pages/SignUp'
+import Blogs from './pages/Blogs.tsx'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/blog/:id" element={<Blog />} />
+          <Route path="/blogs" element={<Blogs />} />
         </Routes>
       </BrowserRouter>
     </>
