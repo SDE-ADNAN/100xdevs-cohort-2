5a790c83076f889ee413be4449348e94199b5a64
SDE-ADNAN <adnansdeofficial@gmail.com>
2024-07-09 23:55:04 +0530

    now rendering blogs dynamically

--- week-13-1-medium-blog/frontend/src/pages/Blogs.tsx
+++ week-13-1-medium-blog/frontend/src/pages/Blogs.tsx
@@ -1,38 +1,37 @@
+import Appbar from "../components/Appbar"
 import BlogCard from "../components/BlogCard"
+import { useBlogs } from "../hooks"
 
-
+interface BlogRes{
+    title:string;
+    content:string;
+    publishedDate:string;
+    published:string;
+    author:{
+        name:string
+    }
+}
 const Blogs = () => {
-  return (
-    <div className="flex justify-center py-8">
-        <div className="max-w-xl">
-        <BlogCard 
-        authorName="Adnan Khan"
-        title="How an ugly single page website maeks $5000 a month without affiliate marketting"
-        content="How an ugly single page website maeks $5000 a month without affiliate markettingHow an ugly single page website maeks $5000 a month without affiliate marketting"
-        publishedDate="8th july 2024"
-        />
-        <BlogCard 
-        authorName="Adnan Khan"
-        title="How an ugly single page website maeks $5000 a month without affiliate marketting"
-        content="How an ugly single page website maeks $5000 a month without affiliate markettingHow an ugly single page website maeks $5000 a month without affiliate marketting"
-        publishedDate="8th july 2024"
-        />
-        <BlogCard 
-        authorName="Adnan Khan"
-        title="How an ugly single page website maeks $5000 a month without affiliate marketting"
-        content="How an ugly single page website maeks $5000 a month without affiliate markettingHow an ugly single page website maeks $5000 a month without affiliate marketting"
-        publishedDate="8th july 2024"
-        />
-        <BlogCard 
-        authorName="Adnan Khan"
-        title="How an ugly single page website maeks $5000 a month without affiliate marketting"
-        content="How an ugly single page website maeks $5000 a month without affiliate markettingHow an ugly single page website maeks $5000 a month without affiliate marketting"
-        publishedDate="8th july 2024"
-        />
-    </div>
-    </div>
-    
-  )
+    const { loading, blogs } = useBlogs()
+    if(loading){
+        return <div>Loading....</div>
+    }
+    return (
+        <div>
+            <Appbar />
+            <div className="flex justify-center py-8">
+                <div className="w-xl">
+                    {blogs.map((item: BlogRes,index) => <BlogCard
+                        key={`${index}`}
+                        authorName={item.author.name}
+                        title={item.title}
+                        content={item.content}
+                        publishedDate={item.published}
+                    />)}
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Blogs
