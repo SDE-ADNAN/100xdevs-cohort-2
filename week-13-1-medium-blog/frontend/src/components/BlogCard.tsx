5df335e11236a7437e3f0b05b02eee8a9f815991
SDE-ADNAN <adnansdeofficial@gmail.com>
2024-07-09 23:54:09 +0530

    changes in avtar component

--- week-13-1-medium-blog/frontend/src/components/BlogCard.tsx
+++ week-13-1-medium-blog/frontend/src/components/BlogCard.tsx
@@ -43,9 +43,9 @@ w-1 rounded-full bg-slate-400">
     </div>
 }
 
-function Avatar({ name }: { name: string }) {
+export function Avatar({ name,size = 5 }: { name: string ,size?:number}) {
     return (
-        <div className="relative inline-flex items-center justify-center w-5 h-5 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+        <div className={`relative inline-flex items-center justify-center w-${size} h-${size} overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600`}>
             <span className="font-xs font-extralight text-gray-600 dark:text-gray-300">
                 {name.charAt(0).toUpperCase()}
             </span>
