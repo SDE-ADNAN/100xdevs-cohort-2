72a11db4a2005d9394d614a54a4c9d3cb5ad6d63
SDE-ADNAN <adnansdeofficial@gmail.com>
2024-07-10 23:50:47 +0530

    api call for individual blog added

--- week-13-1-medium-blog/frontend/src/hooks/index.ts
+++ week-13-1-medium-blog/frontend/src/hooks/index.ts
@@ -1,25 +1,43 @@
 import axios from "axios";
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import { BACKEND_URL } from "../config";
+import { Blog } from "../pages/Blogs";
 
-export const useBlogs =()=>{
-    const [ loading, setIsLoading] = useState(true);
-    const [ blogs, setBlogs] = useState([]);
-
+export const useBlogs = () => {
+    const [loading, setIsLoading] = useState(true);
+    const [blogs, setBlogs] = useState<Blog[]>();
     const token = localStorage.getItem("token");
-
     const config = {
         headers: { Authorization: `${token}` }
     };
-    useEffect(()=>{
-        axios.get(`${BACKEND_URL}/api/v1/blog/bulk`,config).then(response=>{
+    useEffect(() => {
+        axios.get(`${BACKEND_URL}/api/v1/blog/bulk`, config).then(response => {
             setBlogs(response.data.blogs);
             setIsLoading(false);
         })
-    },[])
-
+    }, [])
     return {
         loading,
         blogs
+    }
+}
+
+
+export const useBlog = (id: string) => {
+    const [loading, setIsLoading] = useState(true);
+    const [blog, setBlog] = useState<Blog>();
+    const token = localStorage.getItem("token");
+    const config = {
+        headers: { Authorization: `${token}` }
+    };
+    useEffect(() => {
+        axios.get(`${BACKEND_URL}/api/v1/blog/${id}`, config).then(response => {
+            setBlog(response.data.blog);
+            setIsLoading(false);
+        })
+    }, [])
+    return {
+        loading,
+        blog
     }
 }
