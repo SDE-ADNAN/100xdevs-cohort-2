8165ca7a34159675b99b7ad18a709a1e14e0963b
SDE-ADNAN <adnansdeofficial@gmail.com>
2024-07-14 01:34:08 +0530

    refactor

--- week-13-1-medium-blog/frontend/src/components/Auth.tsx
+++ week-13-1-medium-blog/frontend/src/components/Auth.tsx
@@ -42,13 +42,13 @@ const Auth = ({ type }: { type: "signup" | "signin" }) => {
                     <div className="pt-8">
                         {type === "signup" ?
                             <LabelledInput label='Name' placeholder="Adnan Khan..." onChange={(e) => {
-                                setPostInputs((state) => ({ ...state, name: e.target.value }))
+                                setPostInputs((state: SignupInput) => ({ ...state, name: e.target.value }))
                             }} /> : ""}
                         <LabelledInput label='Username' placeholder="Adnan3621" onChange={(e) => {
-                            setPostInputs((state) => ({ ...state, username: e.target.value }))
+                            setPostInputs((state: SignupInput) => ({ ...state, username: e.target.value }))
                         }} />
                         <LabelledInput label='Password' type="password" placeholder="******" onChange={(e) => {
-                            setPostInputs((state) => ({ ...state, password: e.target.value }))
+                            setPostInputs((state: SignupInput) => ({ ...state, password: e.target.value }))
                         }} />
                         <button type="button" onClick={request} className="mt-8 w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">{type === "signup" ? "Sign Up" : "Sign In"}</button>
                     </div>
