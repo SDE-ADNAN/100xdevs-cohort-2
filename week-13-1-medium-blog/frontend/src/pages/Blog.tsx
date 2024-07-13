1d12d94d618e94a7954b04d01ce1dd1c62660433
SDE-ADNAN <adnansdeofficial@gmail.com>
2024-07-14 01:21:26 +0530

    added spinner in to ui

--- week-13-1-medium-blog/frontend/src/pages/Blog.tsx
+++ week-13-1-medium-blog/frontend/src/pages/Blog.tsx
@@ -1,12 +1,21 @@
 import { useParams } from "react-router-dom"
 import { useBlog } from "../hooks"
 import { FullBlog } from "./FullBlog"
+import Appbar from "../components/Appbar"
+import { Spinner } from "../components/Spinner"
 
 const Blog = () => {
   const { id } = useParams()
   const { loading, blog } = useBlog(id || "1")
-  if (loading) {
-    return <div>Loading....</div>
+  if (loading || !blog) {
+    return <div>
+      <Appbar />
+      <div className="h-screen flex flex-col justify-center">
+        <div className="flex justify-center">
+          <Spinner />
+        </div>
+      </div>
+    </div>
   }
   return (
     <div>{blog && <FullBlog blog={blog} />}</div>
