c96f0fc81ede2a9beb46b15ca6f442383a900701
SDE-ADNAN <adnansdeofficial@gmail.com>
2024-07-14 01:21:42 +0530

    added skeleton into ui

--- week-13-1-medium-blog/frontend/src/pages/Blogs.tsx
+++ week-13-1-medium-blog/frontend/src/pages/Blogs.tsx
@@ -1,34 +1,47 @@
 import Appbar from "../components/Appbar"
 import BlogCard from "../components/BlogCard"
+import { BlogSkeleton } from "../components/BlogSkeleton";
 import { useBlogs } from "../hooks"
 
-export interface Blog{
-    id:string;
-    title:string;
-    content:string;
-    publishedDate:string;
-    published:string;
-    author:{
-        name:string
+export interface Blog {
+    id: string;
+    title: string;
+    content: string;
+    publishedDate: string;
+    published: string;
+    author: {
+        name: string
     }
 }
 const Blogs = () => {
     const { loading, blogs } = useBlogs()
-    if(loading){
-        return <div>Loading....</div>
+    if (loading) {
+        return <div>
+            <Appbar />
+            <div className="flex justify-center">
+                <div>
+                    <BlogSkeleton />
+                    <BlogSkeleton />
+                    <BlogSkeleton />
+                    <BlogSkeleton />
+                    <BlogSkeleton />
+                </div>
+            </div>
+        </div>
     }
+
     return (
         <div>
             <Appbar />
             <div className="flex flex-col px-4 justify-center py-8">
-                    {blogs && blogs.map((item: Blog,index:number) => <BlogCard
-                        key={`${index}`}
-                        id={item.id}
-                        authorName={item.author.name}
-                        title={item.title}
-                        content={item.content}
-                        publishedDate={"28th September 2018"}
-                    />)}
+                {blogs && blogs.map((item: Blog, index: number) => <BlogCard
+                    key={`${index}`}
+                    id={item.id}
+                    authorName={item.author.name}
+                    title={item.title}
+                    content={item.content}
+                    publishedDate={"28th September 2018"}
+                />)}
             </div>
         </div>
     )
