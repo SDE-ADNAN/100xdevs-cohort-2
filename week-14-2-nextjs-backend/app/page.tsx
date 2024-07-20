511c01b2ae232e5997023009407c4af1ea398ce3
SDE-ADNAN <adnansdeofficial@gmail.com>
2024-07-21 01:10:37 +0530

    added the loading.tsx file

--- week-14-2-nextjs-backend/app/page.tsx
+++ week-14-2-nextjs-backend/app/page.tsx
@@ -1,6 +1,37 @@
+import axios from "axios";
 
-export default function Home() {
+
+interface Address {
+  city: string;
+  state: string;
+  houseNumber: string;
+}
+
+interface User {
+  name: string;
+  email: string;
+  address: Address;
+}
+
+async function getUserDetails() {
+  await new Promise((r) => setTimeout(r, 5000))
+  const response = await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details")
+  return response.data;
+}
+
+// async component
+export default async function Home() {
+  const userData: User = await getUserDetails();
   return (
-    <div className="text-red-700">hi</div>
+    <div className="flex flex-col justify-center h-screen">
+      <div className="flex justify-center">
+        <div className="border p-8 rounded">
+          <div>
+            Name: {userData?.name}
+          </div>
+          {userData?.email}
+        </div>
+      </div>
+    </div>
   );
 }
