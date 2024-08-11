4db79d4c8523ff68d4848ee07aa315afd6861619
SDE-ADNAN <adnansdeofficial@gmail.com>
2024-08-11 22:53:02 +0530

    added two new components

---
+++ week-16-2/apps/web/app/admin/page.tsx
@@ -0,0 +1,13 @@
+import Admin from '@repo/ui/admin'
+import { InputBox } from '@repo/ui/input-box'
+import React from 'react'
+
+export default function () {
+    return (
+        <div>
+            hi from admin Page
+            <Admin />
+            <InputBox />
+        </div>
+    )
+}
