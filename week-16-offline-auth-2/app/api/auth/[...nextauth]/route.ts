e9b962b4612e7f792477d6b3ba863cf2a0803ead
SDE-ADNAN <adnansdeofficial@gmail.com>
2024-08-25 23:06:21 +0530

    made env and used next auth's signin and signup

--- week-16-offline-auth-2/app/api/auth/[...nextauth]/route.ts
+++ week-16-offline-auth-2/app/api/auth/[...nextauth]/route.ts
@@ -23,6 +23,7 @@ const handler = NextAuth({
       },
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET,
 });
 
 export const GET = handler;
--- week-16-offline-auth-2/components/Appbar.tsx
+++ week-16-offline-auth-2/components/Appbar.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { signIn } from 'next-auth/react';
 import { useRouter } from 'next/navigation'
 import React from 'react'
 
@@ -6,7 +7,8 @@ const Appbar = () => {
     const router = useRouter();
     return (
         <div style={{ width: '100%', padding: '20px', backgroundColor: 'grey' }}>
-            <button onClick={() => router.push('/api/auth/signin')}>Signin</button>
+            <button onClick={() => signIn()}>Sign In</button>
+            <button onClick={() => signIn()}>Logout </button>
         </div>
     )
 }
